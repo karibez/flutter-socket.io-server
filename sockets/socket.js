b4e0a6446c5bc11ad49b1da68020153475d227b7
Karina Benitez <karinabz@hotmail.com>
2024-12-25 15:57:51 -0600

    Cambios sobre bandas terminado

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,11 +1,26 @@
 
 const { io } = require('../index');
+const Bands = require ('../models/bands');
+const Band = require ('../models/band');
+
+ const bands = new Bands();
+
+ 
+
+bands.addBand(new Band( 'Queen' ));
+bands.addBand(new Band( 'Bon Jovi' ));
+bands.addBand(new Band( 'Heroes del silencio' ));
+bands.addBand(new Band( 'Metallica' ));
+
+console.log(bands); 
 
 
 // Mensajes de sockets
 io.on('connection', client => {
     
     console.log('Cliente conectado');
+
+    client.emit('active-bands', bands.getBands());
 
     client.on('disconnect', () => { 
         console.log('Cliente desconectado');
@@ -19,6 +34,26 @@ io.on('connection', client => {
 
      });
 
+     client.on('vote-band', ( payload ) => {
+        
+      bands.voteBand( payload.id );
+      io.emit('active-bands', bands.getBands());
 
+     });
+
+     client.on('add-band', ( payload ) => {
+      console.log('Banda agregada!!! ', payload);
+      const newBand = new Band( payload.name );
+      bands.addBand( newBand );
+      io.emit('active-bands', bands.getBands());
+
+     });
+
+     client.on('delete-band', ( payload ) => {
+        
+      bands.deleteBand( payload.id );
+      io.emit('active-bands', bands.getBands());
+
+     });
 
   });
